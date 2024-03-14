03d3db8daf545a4ea9798d4cb4685446dcc3e31a
echozyr2001 <echo.zyr.2001@gmail.com>
2024-03-14 15:56:50 +0800

    fix: wrong file behavior
    should open file rather than create

--- src/utils.rs
+++ src/utils.rs
@@ -5,7 +5,7 @@ use std::{
 
 pub fn get_code_from_file(file_path: &str) -> io::Result<String> {
   let mut lua_code = String::new();
-  let mut file = File::create(file_path)?;
+  let mut file = File::open(file_path)?;
   file.read_to_string(&mut lua_code)?;
   Ok(lua_code)
 }
